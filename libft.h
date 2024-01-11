1510e1980adb3665c251e19a72090b193c4c842f
Susana Natali Juan Madriz <sjuan-ma@c2r5s6.42madrid.com>
2024-01-11 12:30:32 +0100

    Guevo

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjuan-ma <sjuan-ma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 12:18:39 by sjuan-ma          #+#    #+#             */
+/*   Updated: 2024/01/11 10:28:21 by sjuan-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+#endif
